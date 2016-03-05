00c8a22311721049a8ab42267ceeab33df0d9083
LBW <LBW@192.168.33.65>
2016-03-05 11:45:50 +0100

    move object

--- Astroidz/src/joshi/astroidz/trailer/motion.java
+++ Astroidz/src/joshi/astroidz/trailer/motion.java
@@ -23,10 +23,94 @@
  */
 package joshi.astroidz.trailer;
 
+import javafx.scene.paint.Color;
+
 /**
  *
  * @author Dano & Joshua
  */
 public class motion {
+   private JFrame frame;
+  private  MalenPanel mp; 
+   private int x =1;
+  private int step =1;
+  private int stepy =1;
+  private int stepl=1;
+  private int stepr=1;
+  private int durchmesser =20;
+  private int y=1;
+    
+    
+    public void los(){
+    frame = new JFrame("Astroiz");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    
+    mp = new MalenPanel();
+    frame.add(mp);
+    
+    frame.addKeyListener(new MKeyListener());
+    
+    frame.setSize(500,400);
+    frame.setVisible(true);
+    
+    while (true) { 
+      x=x+ step;
+      y=y+ stepy;
+      r=r+addr;
+      l=l+addl;
+      
+      mp.repaint();
+      try { 
+        Thread.sleep(9);
+      } catch(Exception e) {
+        
+      } 
+      if (x>mp.getWidth()-durchmesser) {
+        step=-1;
+        //stepy=0;
+      } // end of if
+      if (x<0) {
+        step =0;
+        stepy=0;
+      } // end of if
+      if (y>mp.getHeight()-durchmesser) {
+        stepy=-1;
+      } // end of if
+      if (y<0) {
+        stepy =1;
+      } // end of if
+      
+    
+    }
+    public class MalenPanel extends JPanel{
+    public void paintComponent(Graphics g){
+      g.setColor(Color.RED);
+      g.fillRect(0,0,mp.getWidth(),mp.getHeight());
+      g.setColor(Color.RED);
+      
+      
+    }
+  }
+    
+    
+    
+     public class MKeyListener implements KeyListener{
+     public void keyTyped(KeyEvent e){
+      char c = e.getKeyChar();
+    }
+    public void keyPressed(KeyEvent e){
+      char c =e.getKeyChar();
+      if (c=='w') addl=  -3; 
+      if (c=='a') addl=   3;
+      if (c=='s') addr=  -3;
+      if (c=='d') addr=   3;
+    }
+    public void keyReleased(KeyEvent e){
+      char c =e.getKeyChar();
+      if (c=='w') addl=   0; 
+      if (c=='a') addl=   0;
+      if (c=='s') addr=   0;
+      if (c=='d') addr=   0;
+    }
     
 }
