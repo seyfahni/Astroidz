4c80536a95432a730f1bc36f5500a08f285ccd97
Joshua <Joshua@Bergziege.fritz.box>
2016-03-07 21:16:57 +0100

    Finished move of Motion.java.

--- Astroidz/src/joshi/astroidz/demo/Motion.java
+++ Astroidz/src/joshi/astroidz/demo/Motion.java
@@ -21,7 +21,7 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
  */
-package joshi.astroidz.trailer;
+package joshi.astroidz.demo;
 
 import java.awt.*;
 import javax.swing.*;
