e0168c1aebe182e5116feb2faf5006abee67b29f
Niklas Seyfarth <niklas@seyfarth.de>
2016-06-20 14:42:32 +0200

    Fix error by moving class declaration out of method.

--- src/main/java/joshi/astroidz/main/Background.java
+++ src/main/java/joshi/astroidz/main/Background.java
@@ -12,31 +12,35 @@ package joshi.astroidz.main;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+
 public class Background {
-  
-    JFrame frame = new JFrame();
-    ZeichenPanel m = new ZeichenPanel();
-      Image[] imageArray = new Image[1];
-     
-     
-    public static void main(String[] args) {
-    
-    Background b = new Background();
-    b.los();
-  } // end of main
-    public void los(){
-     JFrame frame =  new JFrame("Animation");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    m = new ZeichenPanel();
-    frame.add(m);  
-        
-    imageArray[0] = new ImageIcon("Background.jpg").getImage();
-    
-      class ZeichenPanel extends JPanel{
-    public void paintComponent(Graphics g){
-      g.drawImage(imageArray[0],2,2,this);
-        
-     }
-    }
-   } 
+
+	JFrame frame = new JFrame();
+	ZeichenPanel m = new ZeichenPanel();
+	Image[] imageArray = new Image[1];
+
+	public static void main(String[] args) {
+
+		Background b = new Background();
+		b.los();
+	} // end of main
+
+	public void los() {
+		JFrame jframe = new JFrame("Animation");
+		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		m = new ZeichenPanel();
+		jframe.add(m);
+
+		imageArray[0] = new ImageIcon("Background.jpg").getImage();
+
+	}
+
+	class ZeichenPanel extends JPanel {
+		private static final long serialVersionUID = -2949720020476023L;
+
+			@Override
+		public void paintComponent(Graphics g) {
+			g.drawImage(imageArray[0], 2, 2, this);
+		}
+	}
 }
