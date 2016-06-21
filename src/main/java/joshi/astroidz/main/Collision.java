8a40ec61714ebe11d74680a62080eb106088dbf7
DanoSchoner <dano@schoner-net.de>
2016-06-21 15:54:49 +0200

    Format the Collison class.

---
+++ src/main/java/joshi/astroidz/main/Collision.java
@@ -0,0 +1,102 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package joshi.astroidz.main;
+
+/**
+ *
+ * @author 
+ */
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+
+public class Collision extends Collison1 {
+    private int dx;
+    private int dy;
+    private ArrayList<Missile> missiles;
+
+    public Craft(int x, int y) {
+        super(x, y);
+
+        initCraft();
+    }
+
+    private void initCraft() {
+        
+        missiles = new ArrayList<>();
+        loadImage("craft.png");
+        getImageDimensions();
+    }
+
+    public void move() {
+
+        x += dx;
+        y += dy;
+
+        if (x < 1) {
+            x = 1;
+        }
+
+        if (y < 1) {
+            y = 1;
+        }
+    }
+
+    public ArrayList getMissiles() {
+        return missiles;
+    }
+
+    public void keyPressed(KeyEvent e) {
+
+        int key = e.getKeyCode();
+
+        if (key == KeyEvent.VK_SPACE) {
+            fire();
+        }
+
+        if (key == KeyEvent.VK_LEFT) {
+            dx = -1;
+        }
+
+        if (key == KeyEvent.VK_RIGHT) {
+            dx = 1;
+        }
+
+        if (key == KeyEvent.VK_UP) {
+            dy = -1;
+        }
+
+        if (key == KeyEvent.VK_DOWN) {
+            dy = 1;
+        }
+    }
+
+    public void fire() {
+        missiles.add(new Missile(x + width, y + height / 2));
+    }
+
+    public void keyReleased(KeyEvent e) {
+
+        int key = e.getKeyCode();
+
+        if (key == KeyEvent.VK_LEFT) {
+            dx = 0;
+        }
+
+        if (key == KeyEvent.VK_RIGHT) {
+            dx = 0;
+        }
+
+        if (key == KeyEvent.VK_UP) {
+            dy = 0;
+        }
+
+        if (key == KeyEvent.VK_DOWN) {
+            dy = 0;
+        }
+    }
+}
+
+}
